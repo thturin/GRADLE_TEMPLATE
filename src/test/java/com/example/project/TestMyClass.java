9e0880d3372c13f58a08b16c92edc2c355754e76
tati <thturin@users.noreply.github.com>
2026-01-14 17:43:34 -0500

    Delete redundant test cases in TestMyClass
    Removed multiple test cases for Time class.

---
+++ src/test/java/com/example/project/TestMyClass.java
@@ -0,0 +1,20 @@
+package com.example.project;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import java.beans.Transient;
+import java.io.*;
+
+
+public class TestTime {
+    // @Test
+    // public void test(){
+    // }
+   
+   
+}
+
+
+
